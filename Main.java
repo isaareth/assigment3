7fb70fa463239bb439afdcdf10a0ba242df29a30
isaareth <sifloress@eafit.edu.co>
2024-05-10 12:18:51 -0500

    Creado el metodo estatico que imprime mi nombre

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        printAssigment3();
+    }
+
+    public static void printAssigment3() {
+        System.out.println("Assignment 3 - Sofía Isaareth Flores Suárez");
+    }
+}
